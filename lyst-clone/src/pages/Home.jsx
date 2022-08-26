4c27ea944c52e5b2b2769924b9c506db39bde896
Mukul <mukulsomukesh@gmail.com>
2022-08-26 12:04:01 +0530

    products page

--- lyst-clone/src/pages/Home.jsx
+++ lyst-clone/src/pages/Home.jsx
@@ -1,17 +1,19 @@
-import FirstSlider from "../components/slider/FirstSlider";
+import { Text, Select, AccordionIcon, AccordionPanel, Accordion, AccordionButton, AccordionItem, HStack, VStack, Image, Center, Button, Box, InputGroup, Stack, Input, InputLeftElement, Flex, Spacer } from '@chakra-ui/react'
+
+import FirstSlider from "../components/FirstSlider";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Products from "../pages/Products"
 
 function Home(){
     return (
         <>
-        {/* 
-        <FirstSlider></FirstSlider> */}
-        {/* <Navbar></Navbar> */}
-        <Footer></Footer>
+        <Navbar></Navbar>        
+    
+    <Image src="https://cdna.lystit.com/cms/ENG_header_desktop_bddf9b3474_884ff704d3.jpg" borderTop={"1px"} w="100%" h="82vh" />
+<FirstSlider></FirstSlider>
 
-{/* <FirstSlider></FirstSlider> */}
-
+<Footer></Footer>
 
         </>
     );
